9263033b5f9324ba6dd6d19d5f372ba931d9ab67
pure-night <58593218+pure-night@users.noreply.github.com>
2024-01-05 15:49:38 +0900

    ver1

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 이 프로젝트가 COM에 노출되는 경우 다음 GUID는 typelib의 ID를 나타냅니다.
-[assembly: Guid("6889eeec-c20a-4420-ba02-e6b8bdb06b8b")]
+[assembly: Guid("f788b6cb-931c-4491-922e-46da3335d027")]
 
 // 어셈블리의 버전 정보는 다음 네 가지 값으로 구성됩니다.
 //
